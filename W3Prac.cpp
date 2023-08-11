481147fb7691ed36fab9cc527fa6a4fef76d3bea
Amjad-Khan-360 <amjadkhanniazi010@gmail.com>
2023-08-11 23:25:41 +0500

    My first cPP code in this repo

---
+++ W3Prac.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main(){
+    //comment
+    /*Comment
+    2nd comment*/
+    cout << "Hello World!\n\n";
+    cout<<"I am\t Amjad";
+    //variables lesson starts here
+    int myNum=32442;
+    string myName="Amjad Khan Niazi";
+    float myAge=21.9;
+    char myGrade='A';
+    bool myBool=true;
+    //cout<<"\n\n"<<myNum<<endl<<myName<<endl<<myAge<<endl<<myGrade<<endl<<myBool;
+    cout<<"\n\nI'm "<<myAge<<" years old.";
+    //adding numbers
+    double sum=myAge+myNum;
+    cout<<endl<<sum;
+    return 0;
+}
