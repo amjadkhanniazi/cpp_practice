23d04a7c05e36c2ffe0c9b31c55c1e973e2c382f
Amjad-Khan-360 <amjadkhanniazi010@gmail.com>
2023-08-12 22:10:04 +0500

    day2

---
+++ Variables.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+const int minPerHour=60;
+const float PI=3.1415;
+int main(){
+    // int a=3,b=4,c=5;
+    // a=b=c=50;
+    // cout<<a+b+c;
+    // cout<<endl<<minPerHour;
+    // cout<<endl<<PI;
+
+    //input in C++
+    // int x;
+    // cout<<"\n\nEnter a number: ";
+    // cin>>x;
+    // cout<<"You entered "<<x;
+    //sum calcular
+    // int num1,num2;
+    // cout<<"\n\nEnter first number: ";
+    // cin>>num1;
+    // cout<<"\nEnter Second Number: ";
+    // cin>>num2;
+    // int sum=num1+num2;
+    // cout<<"\nSum is: "<<sum;
+    // float s1=34e3;
+    // double s2=34E12;
+    // cout<<s1;
+    // cout<<endl<<s2;
+    // int x=1;
+    // bool y=false;
+    // cout<<x+y;
+    char a = 65, b = 66, c = 67;
+    cout<<a;
+    cout<<endl<<b;   
+    cout<<endl<<c;
+    return 0;
+}
