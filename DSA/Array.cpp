7e48d5778a2b7e3ffc992bb5adb8f24961b9c0a2
Amjad-Khan-360 <amjadkhanniazi010@gmail.com>
2023-08-28 17:57:41 +0500

    Functions added to array class

--- DSA/Array.cpp
+++ DSA/Array.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 class arrays{
 public:
+//reverse array
 void arrayReverse(int arr[],int start,int end){
     int temp=0;
     while(start<end){
@@ -12,6 +13,25 @@ void arrayReverse(int arr[],int start,int end){
         end--;
     }
 }
+//Reverse Array Using Reccursion
+void arrayReverseReccursion(int arr[],int start, int end){
+    if(start>=end){
+        return;
+    }
+    int temp=arr[start];
+    arr[start]=arr[end];
+    arr[end]=temp;
+    arrayReverseReccursion(arr,start+1,end-1);
+}
+//Array Rotation
+void arrayRotationRightShift(int arr[],int start, int end){
+    int temp=arr[end];
+    for(int i=end;i>start;i--){
+        arr[i]=arr[i-1];
+    }
+    arr[start]=temp;
+}
+//print array
 void printArray(int arr[],int end){
     int i=0;
     while(i<end){
@@ -20,15 +40,23 @@ void printArray(int arr[],int end){
     }
 }
 };
+//main function
 int main(){
     int arr[]={1,2,3,4,5};
     int end=sizeof(arr)/sizeof(int);
     
     int start=0;
     arrays myArray;
-    myArray.printArray(arr,end);
 
+    myArray.printArray(arr,end);
+    //reverse array
     myArray.arrayReverse(arr,start,end-1);
+    myArray.printArray(arr,end);
+    //Reverse Array Using Reccursion
+    myArray.arrayReverseReccursion(arr,start,end-1);
+    myArray.printArray(arr,end);
+    //Array Rotation
+    myArray.arrayRotationRightShift(arr,start,end-1);
     myArray.printArray(arr,end);
     return 0;
 }
